a35eb208db7827bc5781cfe59c7a15b2fb0b4b59
정도영 <dyj@ino-on.com>
2018-09-28 17:10:37 +0900

    Solve baekjoon p10998

---
+++ algorithm/src/main/java/baekjoon/p10998/Main.kt
@@ -0,0 +1,11 @@
+package baekjoon.p10998
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    println(a * b)
+}
