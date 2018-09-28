19be3e4ee87c7577fd4b26cef8a3dc59259017c3
정도영 <dyj@ino-on.com>
2018-09-28 17:01:26 +0900

    Solve baekjoon p02557

---
+++ algorithm/src/main/java/baekjoon/p02557/Main.java
@@ -0,0 +1,9 @@
+package baekjoon.p02557;
+
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ algorithm/src/main/java/baekjoon/p02557/Main.kt
@@ -0,0 +1,5 @@
+package baekjoon.p02557
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
