f7da9ca947c8340ba7de12a285de4f19d518a012
정도영 <dyj@ino-on.com>
2018-10-01 21:36:00 +0900

    BAEKJOON 03046

---
+++ algorithm/src/main/java/baekjoon/p03046/Main.kt
@@ -0,0 +1,12 @@
+package baekjoon.p03046
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val r1 = sc.nextInt()
+    val s = sc.nextInt()
+    val r2 = (2 * s) - r1
+
+    println(r2)
+}
