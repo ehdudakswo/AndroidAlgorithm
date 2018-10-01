90b2fa0aa256088a6e0c73c899803862d34d6c2a
정도영 <dyj@ino-on.com>
2018-10-01 21:13:56 +0900

    BAEKJOON 02558

---
+++ algorithm/src/main/java/baekjoon/p02558/Main.kt
@@ -0,0 +1,11 @@
+package baekjoon.p02558
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    println(a + b)
+}
