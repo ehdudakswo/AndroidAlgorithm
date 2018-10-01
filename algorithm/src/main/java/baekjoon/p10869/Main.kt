4c7f85f7221f767dfd914597d64b7c6fa2447deb
정도영 <dyj@ino-on.com>
2018-10-01 20:50:07 +0900

    BAEKJOON 10869

---
+++ algorithm/src/main/java/baekjoon/p10869/Main.kt
@@ -0,0 +1,15 @@
+package baekjoon.p10869
+
+import java.util.*
+
+fun main(args : Array<String>) {
+    val sc = Scanner(System.`in`)
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
