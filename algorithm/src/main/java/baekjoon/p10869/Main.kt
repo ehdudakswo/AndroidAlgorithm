f798b2618eae47660d7a39fcef983f5148f07d1b
정도영 <dyj@ino-on.com>
2018-10-02 21:17:46 +0900

    Reformat Code

--- algorithm/src/main/java/baekjoon/p10869/Main.kt
+++ algorithm/src/main/java/baekjoon/p10869/Main.kt
@@ -2,7 +2,7 @@ package baekjoon.p10869
 
 import java.util.*
 
-fun main(args : Array<String>) {
+fun main(args: Array<String>) {
     val sc = Scanner(System.`in`)
     val a = sc.nextInt()
     val b = sc.nextInt()
