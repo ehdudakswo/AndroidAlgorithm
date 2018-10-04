77feb4895454450ab77ebac8e8d4c9f598ea121d
정도영 <dyj@ino-on.com>
2018-10-04 20:32:13 +0900

    BAEKJOON: 07287

---
+++ algorithm/src/main/java/baekjoon/p07287/Main.java
@@ -0,0 +1,10 @@
+package baekjoon.p07287;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(198);
+        System.out.println("do01124");
+    }
+
+}
---
+++ algorithm/src/main/java/baekjoon/p07287/Main.kt
@@ -0,0 +1,6 @@
+package baekjoon.p07287
+
+fun main(args: Array<String>) {
+    println(198)
+    println("do01124")
+}
