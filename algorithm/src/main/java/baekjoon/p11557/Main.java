c2f384df9fb313a07240a0137168d706fcb79573
정도영 <dyj@ino-on.com>
2018-11-01 21:51:35 +0900

    BAEKJOON: 11557

---
+++ algorithm/src/main/java/baekjoon/p11557/Main.java
@@ -0,0 +1,57 @@
+package baekjoon.p11557;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            P11557[] input = new P11557[n];
+
+            for (int i = 0; i < input.length; i++) {
+                input[i] = new P11557(sc.next(), sc.nextInt());
+            }
+
+            System.out.println(solve(input));
+        }
+    }
+
+    private static String solve(P11557[] input) {
+        Arrays.sort(input, new Reverse());
+        return input[0].getS();
+    }
+
+}
+
+class P11557 {
+
+    private String s;
+    private Integer l;
+
+    P11557(String s, int l) {
+        this.s = s;
+        this.l = l;
+    }
+
+    String getS() {
+        return s;
+    }
+
+    Integer getL() {
+        return l;
+    }
+
+}
+
+class Reverse implements Comparator<P11557> {
+
+    @Override
+    public int compare(P11557 obj1, P11557 obj2) {
+        return obj2.getL().compareTo(obj1.getL());
+    }
+
+}
---
+++ algorithm/src/main/java/baekjoon/p11557/Main.kt
@@ -0,0 +1,18 @@
+package baekjoon.p11557
+
+fun main(args: Array<String>) {
+    repeat(readLine()!!.toInt()) {
+        val n = readLine()!!.toInt()
+        val inputs = ArrayList<P11557Kt>(n)
+
+        repeat(n) {
+            val input = readLine()!!.split(" ")
+            inputs.add(P11557Kt(input[0], input[1].toInt()))
+        }
+
+        val max = inputs.maxBy { input -> input.l }
+        println(max!!.s)
+    }
+}
+
+class P11557Kt(val s: String, val l: Int)
