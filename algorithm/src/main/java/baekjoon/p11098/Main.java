1f85bcdc380d3a757c9749ad522c0881a35ae9b9
정도영 <dyj@ino-on.com>
2018-11-04 16:56:45 +0900

    BAEKJOON: p11098

---
+++ algorithm/src/main/java/baekjoon/p11098/Main.java
@@ -0,0 +1,57 @@
+package baekjoon.p11098;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while (n-- > 0) {
+            int p = sc.nextInt();
+            P11098[] inputs = new P11098[p];
+
+            for (int i = 0; i < inputs.length; i++) {
+                inputs[i] = new P11098(sc.nextInt(), sc.next());
+            }
+
+            System.out.println(solve(inputs));
+        }
+    }
+
+    private static String solve(P11098[] inputs) {
+        Arrays.sort(inputs, new PriceComparator());
+        return inputs[inputs.length - 1].getName();
+    }
+
+}
+
+class P11098 {
+
+    private Integer price;
+    private String name;
+
+    P11098(int price, String name) {
+        this.price = price;
+        this.name = name;
+    }
+
+    Integer getPrice() {
+        return price;
+    }
+
+    String getName() {
+        return name;
+    }
+
+}
+
+class PriceComparator implements Comparator<P11098> {
+
+    @Override
+    public int compare(P11098 obj, P11098 obj2) {
+        return obj.getPrice().compareTo(obj2.getPrice());
+    }
+
+}
---
+++ algorithm/src/main/java/baekjoon/p11098/Main.kt
@@ -0,0 +1,17 @@
+package baekjoon.p11098
+
+fun main(args: Array<String>) {
+    repeat(readLine()!!.toInt()) {
+        val p = readLine()!!.toInt()
+        val inputs = ArrayList<P11098Kt>()
+
+        repeat(p) {
+            val input = readLine()!!.split(" ")
+            inputs.add(P11098Kt(input[0].toInt(), input[1]))
+        }
+
+        println(inputs.maxBy { P11098Kt -> P11098Kt.price }!!.name)
+    }
+}
+
+class P11098Kt(val price: Int, val name: String)
