1553bedb34eff6c5a3dabd812c3df4e60705df25
정도영 <dyj@ino-on.com>
2018-11-13 20:58:04 +0900

    BAEKJOON: 02522

---
+++ algorithm/src/main/java/baekjoon/p02522/Main.java
@@ -0,0 +1,40 @@
+package baekjoon.p02522;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(solve(sc.nextInt()));
+    }
+
+    private static String solve(int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 1; i <= n; i++) {
+            sb.append(repeat(" ", n - i));
+            sb.append(repeat("*", i));
+            sb.append("\n");
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            sb.append(repeat(" ", n - i));
+            sb.append(repeat("*", i));
+            sb.append("\n");
+        }
+
+        return sb.toString();
+    }
+
+    private static String repeat(String str, int cnt) {
+        StringBuilder sb = new StringBuilder();
+
+        while (cnt-- > 0) {
+            sb.append(str);
+        }
+
+        return sb.toString();
+    }
+
+}
