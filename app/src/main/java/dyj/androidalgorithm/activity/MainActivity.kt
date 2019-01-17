1056ec17c29afd8cd75f95f0d3e98ed7f66fe01d
정도영 <ehdudakswo@gmail.com>
2019-01-17 14:11:22 +0900

    ANDROID: Add Firebase Storage

--- app/src/main/java/dyj/androidalgorithm/activity/MainActivity.kt
+++ app/src/main/java/dyj/androidalgorithm/activity/MainActivity.kt
@@ -1,34 +1,50 @@
 package dyj.androidalgorithm.activity
 
+import android.Manifest
 import android.os.Bundle
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
-import com.google.firebase.firestore.FirebaseFirestore
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import dyj.androidalgorithm.R
 import dyj.androidalgorithm.adapter.MainRecyclerViewAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main_content.*
 import kotlinx.android.synthetic.main.activity_main_nav_view_test.*
 import kotlinx.android.synthetic.main.activity_main_toolbar.*
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val user = "dyj"
-    private val document = "main_recycler_view"
-    private val type = "item"
     private val recyclerViewAdapter = MainRecyclerViewAdapter(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        checkPermission()
         setNavigationView()
         setOnClickListener()
         setRecyclerView()
-        loadFirebaseFirestore()
+    }
+
+    private fun checkPermission() {
+        TedPermission.with(this)
+                .setPermissionListener(permissionListener)
+                .setPermissions(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .setPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                .check()
+    }
+
+    private val permissionListener: PermissionListener = object : PermissionListener {
+        override fun onPermissionGranted() {
+        }
+
+        override fun onPermissionDenied(deniedPermissions: ArrayList<String>?) {
+        }
     }
 
     private fun setNavigationView() {
@@ -49,23 +65,7 @@ class MainActivity : AppCompatActivity() {
     private fun setRecyclerView() {
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = recyclerViewAdapter
-    }
-
-    private fun loadFirebaseFirestore() {
-        FirebaseFirestore.getInstance().collection(user).document(document).collection(type)
-                .get()
-                .addOnCompleteListener { task ->
-                    when (task.isSuccessful) {
-                        true -> {
-                            recyclerViewAdapter.refresh(task.result!!.documents)
-                        }
-                        false -> {
-                            Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
-                            finish()
-                        }
-                    }
-                }
-
+        recyclerViewAdapter.refresh()
     }
 
 }
--- app/src/main/java/dyj/androidalgorithm/adapter/MainRecyclerViewAdapter.kt
+++ app/src/main/java/dyj/androidalgorithm/adapter/MainRecyclerViewAdapter.kt
@@ -1,45 +1,119 @@
 package dyj.androidalgorithm.adapter
 
+import android.app.ProgressDialog
 import android.content.Context
+import android.os.Environment
+import android.os.Environment.DIRECTORY_DOWNLOADS
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import dyj.androidalgorithm.R
+import java.io.File
 
 class MainRecyclerViewAdapter(private val context: Context)
     : RecyclerView.Adapter<MainRecyclerViewHolder>() {
 
+    //RecyclerView
     private val itemList = ArrayList<Item>()
+    private lateinit var progressDialog: ProgressDialog
+
+    //Firebase Firestore Info
+    private val user = "dyj"
+    private val document = "main_recycler_view"
+    private val type = "item"
+
+    //Firebase Firestore Item Field
     private val date = "date"
-    private val title = "title"
-    private val content = "content"
+    private val path = "path"
+    private val size = "size"
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainRecyclerViewHolder {
-        return MainRecyclerViewHolder(
-                LayoutInflater.from(context).inflate(R.layout.item_main_recycler_view, parent, false)
-        )
+        val view = LayoutInflater.from(context).inflate(R.layout.item_main_recycler_view, parent, false)
+        view.setOnClickListener {
+            showProgressDialog(itemList[viewType])
+            downloadFile(itemList[viewType])
+        }
+
+        return MainRecyclerViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return itemList.size
-    }
+    override fun getItemCount(): Int = itemList.size
 
     override fun onBindViewHolder(holder: MainRecyclerViewHolder, position: Int) {
         val item = itemList[position]
-        holder.date?.text = item.date
-        holder.title?.text = item.title
-        holder.content?.text = item.content
+        holder.title?.text = filePathSuffix(item)
+        holder.content?.text = item.size
     }
 
-    fun refresh(documents: List<DocumentSnapshot>) {
+    private fun showProgressDialog(item: Item) {
+        progressDialog = ProgressDialog(context)
+        progressDialog.setTitle(filePathSuffix(item))
+        progressDialog.show()
+    }
+
+    private fun downloadFile(item: Item) {
+        val storageRef = FirebaseStorage.getInstance().reference
+        val islandRef = storageRef.child(item.path)
+        val dirPath = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS).absolutePath
+        val localFile = File.createTempFile(
+                filePathPrefix(item),
+                filePathSuffix(item),
+                File(dirPath)
+        )
+
+        islandRef.getFile(localFile).addOnSuccessListener {
+            val renameFile = File(dirPath, filePathSuffix(item))
+            localFile.renameTo(renameFile)
+            progressDialog.dismiss()
+            Toast.makeText(context, "${renameFile.name} - Download Complete", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener { e ->
+            progressDialog.dismiss()
+            Toast.makeText(context, "$e - Download Failed", Toast.LENGTH_SHORT).show()
+        }.addOnProgressListener {
+            val percent = ((it.bytesTransferred.toDouble() / it.totalByteCount) * 100).toInt()
+            progressDialog.setMessage("$percent %")
+        }
+    }
+
+    private fun filePathPrefix(item: Item): String {
+        return item.path.split("/").first()
+    }
+
+    private fun filePathSuffix(item: Item): String {
+        return item.path.split("/").last()
+    }
+
+    fun refresh() {
+        FirebaseFirestore.getInstance().collection(user).document(document).collection(type)
+                .get()
+                .addOnCompleteListener { task ->
+                    when (task.isSuccessful) {
+                        true -> {
+                            refresh(task.result!!.documents)
+                        }
+                        false -> {
+                            Toast.makeText(context, task.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+    }
+
+    private fun refresh(documents: List<DocumentSnapshot>) {
         for (document in documents) {
             itemList.add(Item(
                     document[date].toString(),
-                    document[title].toString(),
-                    document[content].toString()
+                    document[path].toString(),
+                    document[size].toString()
             ))
         }
 
@@ -48,11 +122,10 @@ class MainRecyclerViewAdapter(private val context: Context)
 
 }
 
-private class Item(val date: String, val title: String, val content: String)
+private class Item(val date: String, val path: String, val size: String)
 
 class MainRecyclerViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    val date = itemView.findViewById<TextView?>(R.id.textView_date)
     val title = itemView.findViewById<TextView?>(R.id.textView_title)
     val content = itemView.findViewById<TextView?>(R.id.textView_content)
 
